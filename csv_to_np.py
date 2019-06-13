b424e2ceca7c9db57d3bbc7248273fca02d66668
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-06-13 18:12:40 -0400

    Create csv_to_np.py

---
+++ csv_to_np.py
@@ -0,0 +1,17 @@
+import csv to python
+import numpy as np 
+"
+f=r'K:\GISElec_BusinessSupport\YiFan\Duplicate_sp\duplicate_sp.csv'
+d=','
+n=42
+input are file, delimiter, and column number
+"
+#function to convert sp table to numpy array
+def csv_to_np(file,delim,n_col)
+    with open(file, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter="{}".format(delim))
+        ary_list=[]
+        for row in spamreader:
+            ary=np.array(row) if len(row) = n_col else None
+            ary_list.append(ary)
+    return np.array(ary_list)
