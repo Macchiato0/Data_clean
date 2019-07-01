473f759356566a94007c724829387ebfa0e93f0c
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-07-01 14:53:39 -0400

    Update and rename address_extract.py to address.py

---
+++ address.py
@@ -0,0 +1,56 @@
+@check_street
+def address_extract(t): #t is a string '........'
+    match=re.search('\d\w+', t)
+    text=re.split("\s", t)
+    if match:
+        x=match.group() #find the digit part in the string as street number
+        if len(x)<6:
+            position=text.index(x)    
+            if position<(len(text)-2):
+                street=text[position:position+4]
+                return [re.sub(r'[^\w\s]','',s) for s in street]
+               
+    
+street_kw=['RD','TR','AVE','ST','CT','DR','E','W','S','N','SE','NE','SW','NW']
+
+"""
+us street address structure
+
+street number (1 word), street name (1-2 words), direction|surfix (1-2 words)
+
+maximum 5 words
+"""    
+#decorat of address_extract(t)
+#che_street can test if the last words in street list are amaong street key word
+#if the last word is not adn address keyword, the full address will not include the last word 
+
+"""
+example constructor of a python decorator
+
+def p_decorate(func):
+   def func_wrapper(name):
+       return "<p>{0}</p>".format(func(name))
+   return func_wrapper
+
+"""
+
+
+def check_street(func): #s is the list generated from address_extract
+    def func_wrapper(s):
+        list=func(s)
+        sps=" "
+        if list[-1] in street_kw:
+            return sps.join(list)
+        else:
+            return sps.join(list[:-1])
+    return func_wrapper
+
+
+
+"""
+select the address ignor case
+SQL:
+SELECT * FROM myTable WHERE UPPER(field) LIKE UPPER('%string2%')
+""""
+
+
