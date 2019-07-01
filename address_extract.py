608675a6d123a326cf8ca6d2d05c1a7a617fa26e
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-07-01 13:18:48 -0400

    Update address_extract.py

--- address_extract.py
+++ address_extract.py
@@ -1,9 +1,9 @@
 def address_extract(t): #t is a string '........'
     match=re.search('\d\w+', t)
     text=re.split("\s", t)
-    if match is True:
+    if match:
         x=match.group() #find the digit part in the string as street number
-        if len(x)<5:
+        if len(x)<6:
             position=text.index(x)    
             if position<(len(text)-2):
                 street=text[position:position+3]
